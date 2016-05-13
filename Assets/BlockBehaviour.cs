9b5afaa6d4bf21a98cbf35f11e333ef28e4dd3ad
Rasie1 <v.kvachev@logistictech.ru>
2016-05-14 01:08:29 +0300

    BlockBehaviour

---
+++ Assets/BlockBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockBehaviour : MonoBehaviour {
+
+    private bool isActivated;
+    public bool IsActivated {
+        get
+        {
+            return IsActivated;
+        }
+        set
+        {
+            if (value)
+            {
+                if (!isActivated)
+                {
+                    isActivated = true;
+                    Activate();
+                }
+            }
+            else
+            {
+                if (isActivated)
+                {
+                    isActivated = false;
+                    Deactivate();
+                }
+            }
+        }
+    }
+
+    protected void Activate() {
+        Debug.Log("Block activated");
+
+    }
+
+    protected void Deactivate()
+    {
+        Debug.Log("Block deactivated");
+    }
+
+	void Start () {
+        IsActivated = false;
+	}
+	
+	void Update () {
+	
+	}   
+
+    void OnMouseDown() {
+        IsActivated = true;
+    }
+}
