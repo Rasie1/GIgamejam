8b9ccbe5b19d8b6193800ce3b0858de4e8bf365f
unshame <mrunshame@gmail.com>
2016-05-13 21:58:16 +0300

    test1 scene

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -22,7 +22,8 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+				Debug.Log(Input.GetKey(KeyCode.UpArrow));
+				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump") || Input.GetKey(KeyCode.UpArrow);
             }
         }
 
