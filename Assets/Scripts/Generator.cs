3e8f882a5c32b7719c9b05cd0f222d5748470bc0
SimplePrutik <dimodaktil@yandex.ru>
2016-05-14 10:39:37 +0300

    generation and removing

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Generator : MonoBehaviour {
+    
+    private float height = 0;
+    private Queue<int> ids;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ids = new Queue<int>();
+        for (int i = 0; i < 6; ++i)
+            GenerateChunk();
+    }
+	
+    void GenerateChunk()
+    {
+        for (int j = 0; j < 4; ++j) 
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                ids.Enqueue(Instantiate(Resources.Load("Cube") as GameObject, new Vector3(-2.5f + 0.7f * i, -5 + height, 3), Quaternion.identity).GetInstanceID());
+                //some random shit
+            }
+            height += 0.7f;
+        }
+    }
+
+    void DestroyChunk()
+    {
+        for (int i = 0; i < 32; ++i)
+            Destroy(UnityEditor.EditorUtility.InstanceIDToObject(ids.Dequeue()));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (GameObject.Find("TestPlayer").transform.position.y > height - 15 * 0.7f)
+        {
+            GenerateChunk();
+            DestroyChunk();
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Time.deltaTime * new Vector3(0, 2, 0);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Time.deltaTime * new Vector3(0, -2, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Time.deltaTime * new Vector3(-2, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Time.deltaTime * new Vector3(2, 0, 0);
+    }
+}
