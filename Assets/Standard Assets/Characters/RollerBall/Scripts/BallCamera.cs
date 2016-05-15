a65b9438f9a8ee5cbe93e9bd34eb8238a777268a
unshame <mrunshame@gmail.com>
2016-05-15 06:07:24 +0300

    added skybox

--- Assets/Standard Assets/Characters/RollerBall/Scripts/BallCamera.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/BallCamera.cs
@@ -13,9 +13,9 @@ public class BallCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		newPos.x = transform.position.x;
+		newPos.x = 5.2f;
 		newPos.y = m_Rigidbody.position.y;
-		newPos.z = 0;
+		newPos.z = -0.05f;
 		transform.position = newPos; 
 	}
 }
